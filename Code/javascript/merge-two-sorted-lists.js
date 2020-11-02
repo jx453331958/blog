fa78182a107d27ef921050c96113afd8a6c9e334
jx453331958 <jiangxuan9001@gmail.com>
2020-11-02 14:31:53 +0800

    update

---
+++ Code/javascript/merge-two-sorted-lists.js
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+
+ /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+
+};
