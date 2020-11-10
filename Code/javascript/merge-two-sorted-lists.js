eadf0bd950669c1589a7012eff7c5630f860eabc
jx453331958 <jiangxuan9001@gmail.com>
2020-11-10 10:01:36 +0800

    update

--- Code/javascript/merge-two-sorted-lists.js
+++ Code/javascript/merge-two-sorted-lists.js
@@ -3,7 +3,7 @@
  * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
  */
 
- /**
+/**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
  *     this.val = (val===undefined ? 0 : val)
@@ -15,6 +15,27 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
+var mergeTwoLists = function (l1, l2) {
+  const newList = {
+    val: -1,
+    next: null,
+  };
 
+  let tempList = newList;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      tempList.next = l1;
+      l1 = l1.next;
+    } else {
+      tempList.next = l2;
+      l2 = l2.next;
+    }
+
+    tempList = tempList.next;
+  }
+
+  tempList.next = l1 || l2;
+
+  return newList.next;
 };
